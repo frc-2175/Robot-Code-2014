8e7fdb66a3e99fc27e95db177cc3abe836e180e7
Leonard <info@fightingcalculators.org>
2014-02-02 14:05:26 -0800

    Base Auton Behavior
    Encoders added to drivetrain. Two command groups created to run
    different auton routines. Both command groups are default code.
    
    Need to add encoder-based driving capabilities and complete auton
    routines.

--- Commands/Auton1MobilityBonus.h
+++ Commands/Auton1MobilityBonus.h
@@ -9,26 +9,20 @@
 // it from being updated in th future.
 
 
-#ifndef AUTONOMOUS_COMMAND_H
-#define AUTONOMOUS_COMMAND_H
 
+#ifndef AUTON_1_MOBILITY_BONUS_H
+#define AUTON_1_MOBILITY_BONUS_H
 
-#include "Commands/Subsystem.h"
-#include "../Robot.h"
+#include "Commands/CommandGroup.h"
 
 /**
  *
  *
  * @author ExampleAuthor
  */
-class AutonomousCommand: public Command {
-public:
-	AutonomousCommand();
-	virtual void Initialize();
-	virtual void Execute();
-	virtual bool IsFinished();
-	virtual void End();
-	virtual void Interrupted();
+class Auton1MobilityBonus: public CommandGroup {
+public:	
+	Auton1MobilityBonus();
 };
 
 #endif
